38617347be9442971d72603a423fc145f7303bfb
udarensamolet <udarensamolet@yahoo.com>
2025-02-09 18:17:32 +0200

    Added Footer Component

---
+++ frontend/src/app/components/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css'],
+  standalone: true,
+  imports: [MatToolbarModule]
+})
+export class FooterComponent { }
